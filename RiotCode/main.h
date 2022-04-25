241f0bf1191f0f26026358b5028f1625fe6807ed
Andrea <andrear632@gmail.com>
2022-04-25 09:14:17 +0200

    moved define

--- RiotCode/main.h
+++ RiotCode/main.h
@@ -10,4 +10,17 @@ int loramac_setup(char *deui, char *aeui, char *akey, char *xdr);
 int loramac_send(char *message);
 void components_init(void);
 
+//define WASTE_TYPE = PLASTIC | METAL | GLASS | PAPER | FOOD | MIXED
+#define WASTE_TYPE MIXED
+//define LoRa configuration parameters
+#define DEVEUI "70B3D57ED004E9C9"
+#define APPEUI "0000000000000000"
+#define APPKEY "ACF56E9005262992A0D06E5C42192FD7"
+//define bin id
+#define BIN_ID 0
+//define bin height
+#define MAX_DISTANCE 45
+//define area in square meters
+#define AREA 0.05
+
 #endif
