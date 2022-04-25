ad8f809d9849775e77f7a70a010cbb316472f442
Andrea <andrear632@gmail.com>
2022-04-25 09:19:00 +0200

    define updated

--- RiotCode/main.h
+++ RiotCode/main.h
@@ -11,7 +11,7 @@ int loramac_send(char *message);
 void components_init(void);
 
 //define WASTE_TYPE = PLASTIC | METAL | GLASS | PAPER | FOOD | MIXED
-#define WASTE_TYPE MIXED
+#define WASTE_TYPE "MIXED"
 //define LoRa configuration parameters
 #define DEVEUI "70B3D57ED004E9C9"
 #define APPEUI "0000000000000000"
@@ -22,5 +22,12 @@ void components_init(void);
 #define MAX_DISTANCE 45
 //define area in square meters
 #define AREA 0.05
+
+#define WASTE_TYPE_PLASTIC "PLASTIC"
+#define WASTE_TYPE_METAL "METAL"
+#define WASTE_TYPE_GLASS "GLASS"
+#define WASTE_TYPE_PAPER "PAPER"
+#define WASTE_TYPE_FOOD "FOOD"
+#define WASTE_TYPE_MIXED "MIXED"
 
 #endif
